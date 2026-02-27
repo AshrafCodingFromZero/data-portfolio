1104825b8f362f84460713e968c004fefc8be21f
AshrafCodingFromZero <muhammadashrafbinzahari2007@gmail.com>
2026-02-27 21:23:27 +0800

    Create W1_sql_basics.sql

---
+++ sql/W1_sql_basics.sql
@@ -0,0 +1,18 @@
+-- Week 1: SQL Basics
+-- Goal: Practice SELECT / WHERE / ORDER BY / LIMIT
+-- Dataset used: (fill in after you choose one)
+
+-- 1) View first 10 rows
+-- SELECT * FROM <table> LIMIT 10;
+
+-- 2) Select specific columns
+-- SELECT col1, col2 FROM <table> LIMIT 10;
+
+-- 3) Filter with WHERE
+-- SELECT * FROM <table> WHERE <condition> LIMIT 10;
+
+-- 4) Sort results
+-- SELECT * FROM <table> ORDER BY <col> DESC LIMIT 10;
+
+-- 5) Combine WHERE + ORDER + LIMIT
+-- SELECT * FROM <table> WHERE <condition> ORDER BY <col> DESC LIMIT 10;
